9a82893113a10a58f36d3651d1fcd4a3c3612e1d
Dominosrotas <agigantem@gmail.com>
2026-06-03 23:56:06 +0100

    App

---
+++ api.js
@@ -0,0 +1,36 @@
+exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: ''
+    };
+  }
+
+  const fetch = (...args) => import('node-fetch').then(({default: f}) => f(...args));
+  
+  const response = await fetch('https://api.anthropic.com/v1/messages', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.ANTHROPIC_KEY,
+      'anthropic-version': '2023-06-01'
+    },
+    body: event.body
+  });
+
+  const data = await response.json();
+  
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+};
